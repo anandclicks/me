c13e0002cf7050383d50b2aa916f5c87174b166c
anandclicks <146056018+anandclicks@users.noreply.github.com>
2024-07-09 12:54:11 +0530

    first

---
+++ src/components/PageThree.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function PageThree() {
+  return (
+    <>
+    <div className="page3 w-full min-h-10 bgMain">
+      <div className="container h-full">
+      <div className="row h-full  justify-center">
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/html.png" alt="html" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/css.png" alt="css" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/bootstrap.png" alt="bootstrap" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/tailwind.png" alt="tailwind" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/js.png" alt="js" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/react.png" alt="react" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/node.png" alt="node" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/express.png" alt="express" /></div>
+        <div className="col-lg-1 col-3 skillBox"><img src="../public/skills/mongodb.png" alt="mongo" /></div>
+      </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default PageThree
